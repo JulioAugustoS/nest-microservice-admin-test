ac93c929b89d6ffafb22800b8d9462ce8423506c
Julio Augusto <julioams@icloud.com>
2021-09-28 01:19:02 -0300

    feat: create message communication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`${process.env.MONGO_URL}`),
+    MongooseModule.forFeature([
+      { name: 'Categoria', schema: CategoriaSchema },
+      { name: 'Jogador', schema: JogadorSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        `amqp://${process.env.RABBIT_MQ_USER}:${process.env.RABBIT_MQ_PASS}@${process.env.RABBIT_MQ_IP}:${process.env.RABBIT_MQ_PORT}/smartranking`,
+      ],
+      queue: 'admin-backend',
+    },
+  });
+
+  // eslint-disable-next-line
+  // @ts-ignore
+  await app.listen(() => logger.log('Microservice is listening'));
 }
 bootstrap();
